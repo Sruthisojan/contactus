6a6afc166e84e4095571c89ee052f239fbb6e364
Sruthi Sojan <sruthi.sojan@gmail.com>
2020-10-12 22:38:47 +0530

    Working on API

--- contactus/serializers.py
+++ contactus/serializers.py
@@ -1,3 +1,4 @@
+  
 from rest_framework import serializers
 from contactus.models import Contact
 
--- contactus/views.py
+++ contactus/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .models import Contact
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ContactSerializer
 
 # Create your views here.
+
 def index(request):
+    if request.method=="GET":
+        contactlist=Contact.objects.all()
+        serializer=ContactSerializer(contactlist, many=True)
+        return Response(serializer.data)
+
     return render(request,'index.html')
