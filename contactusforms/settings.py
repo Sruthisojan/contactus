eb62979cc09455d5057a187895a2c20019cacf32
Sruthi Sojan <sruthi.sojan@gmail.com>
2020-10-14 16:35:58 +0530

    Heroku

--- contactusforms/settings.py
+++ contactusforms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$jlh$7r$uwrf=95k&0))@*$^u_#n6&o$y@j8^xllt3j^q+^j_('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'contactusform.herokuapp.com']
 
 
 # Application definition
