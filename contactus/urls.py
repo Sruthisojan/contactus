5e9845475d0c0c76db5118b3749f3efa790877a9
Sruthi Sojan <sruthi.sojan@gmail.com>
2020-10-14 11:13:06 +0530

    Working

--- contactus/urls.py
+++ contactus/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.index,name='index'),
+    path('create', views.create_user,name='create'),
     path('', views.contact, name='contact'),
     url(r'^api/contactus$',views.contact_us,name='contact_us'),  
 ]
--- contactus/views.py
+++ contactus/views.py
@@ -7,7 +7,7 @@
 #from rest_framework import status
 #from .serializers import ContactSerializer
 
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http.response import JsonResponse
 from rest_framework.parsers import JSONParser
 from rest_framework import status
@@ -15,6 +15,7 @@ from django.views.decorators.csrf import csrf_exempt
 from contactus.models import Contact
 from contactus.serializers import ContactSerializer
 from rest_framework.decorators import api_view
+from django.http import HttpResponse 
 # Create your views here.
 
 
@@ -23,7 +24,7 @@ def contact(request):
     template='index.html'
     return render(request,template,{'contactlists':contacts})
 
-
+'''
 def index(request):
     if request.method=="POST":
         contactus=Contact()
@@ -37,35 +38,63 @@ def index(request):
         contactus.subject=subject
         contactus.save()
     return render(request,'index.html')
-@csrf_exempt
+    '''
+def create_user(request):
+    
+    if request.method=='POST':
+        name= request.POST['name']
+        email=request.POST['email']
+        num=request.POST['num']
+        subject=request.POST['subject']
+
+        Contact.objects.create(
+
+            name=name,
+            email=email,
+            num=num,
+            subject=subject
+
+        )
+        return HttpResponse("Success")
+
+
 @api_view(["GET","POST"])
 def contact_us(request):
     if request.method=="GET":
         contactus=Contact.objects.all()
         name=request.GET.get('name',None)
         if name is not None:
-            contactus=contactus.filter(name__icontains=name)
+            contactus=contactus.filter(name_icontains=name)
         serializer=ContactSerializer(contactus, many=True)
         return JsonResponse(serializer.data,safe=False)
     
     elif request.method=="POST":
-        print(request.data)
-        try:
-            contactdata=JSONParser().parse(request)
-        except Exception as e:
-            print(e)
-        
-        print(contactdata)
+        contactdata=JSONParser().parse(request)  
         serializer=ContactSerializer(data=contactdata)
-        
-        print(serializer)
         if serializer.is_valid():
             serializer.save()
-            print(serializer.data)
             return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
-        
-        print (serializer.errors)
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    '''@api_view(['GET','POST'])
+
+def contactDetails(request):
+    if request.method == 'GET':
+        contacts = Contact.objects.all()
+        name = request.GET.get('name', None)
+        if name is not None:
+            contacts = contacts.filter(name_icontains = name)
+        contactSerial = ContactusSerializer(contacts, many= True)
+        return JsonResponse(contactSerial.data, safe = False)
+    
+    elif request.method == 'POST':
+        contactData = JSONParser().parse(request)
+        contactSerial = ContactusSerializer(data = contactData)
+        if contactSerial.is_valid():
+            contactSerial.save()
+            return JsonResponse(contactSerial.data, status = status.HTTP_201_CREATED)
+        return JsonResponse(contactSerial.errors, status = status.HTTP_400_BAD_REQUEST)
+'''
     
 
 
