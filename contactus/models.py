34948780f1190cf3be51d6392ad070d421b7dbdc
Sruthi Sojan <sruthi.sojan@gmail.com>
2020-10-16 00:04:19 +0530

    BigIntegerField

--- contactus/models.py
+++ contactus/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Contact(models.Model):
     name=models.CharField(max_length=100)
     email=models.EmailField()
-    num=models.CharField(default='No phone number',max_length=10)
+    num=models.BigIntegerField()
     subject=models.TextField()
     
     class Meta:
