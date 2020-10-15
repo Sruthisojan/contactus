97f50d76ec324cc59f6613b3ddae71489f84fa9a
Sruthi Sojan <sruthi.sojan@gmail.com>
2020-10-15 13:36:36 +0530

    chnged settings and urls

--- contactusforms/settings.py
+++ contactusforms/settings.py
@@ -131,7 +131,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 db_from_env=dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
